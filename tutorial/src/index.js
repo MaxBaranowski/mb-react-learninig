fea1dd688f6986f321e9d0f90fbb08b98c7d1d86
Maksim Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-05 14:54:45 +0100

    up JS

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,51 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+//js in jsx
+function Person(params) {
 
-//stateless Component
-const Greeting = () => {
-  return <h1>Hello from React</h1>
+  const btn = "search button";
+  const name = "john";
+  const lastName = "doe";
+
+  return <section>
+    <button>{btn}</button>
+    <h2>{`${name} ${lastName}`}</h2>
+    <p>{lastName}</p>
+  </section>;
 }
-
-//statefull Component
-class Greeting2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-    this.iter = 0;
-    console.log("constructor" + this.iter)
-  }
-
-  componentWillMount() {
-    this.iter = 2;
-    console.log("componentWillMount" + this.iter)
-  }
-
-  render() {
-    // this.iter = 3;
-    console.log("render" + this.iter)
-    return (
-      <React.Fragment>
-        <Greeting />
-        <h1>Hello from React2 -- {this.iter}</h1>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Click me
-        </button>
-      </React.Fragment>
-    )
-  }
-
-  componentDidMount() {
-    this.iter = 4;
-    console.log("componentDidMount" + this.iter)
-  }
-}
-
-
 
 ReactDOM.render(
-  <Greeting2 />,
+  <Person />,
   document.getElementById('root')
 );
