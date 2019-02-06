2026a86e1377e47e96edd8ab717dbefa6ed7748c
Maksim Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-06 13:40:28 +0100

    file structure

---
+++ app/src/components/user/detailed/components/Nationality.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nationality() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ app/src/services/api/UserAPI.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserAPI() {
+  return (
+    <div>
+
+    </div>
+  )
+}
