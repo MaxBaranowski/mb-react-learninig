0fd0f81ee4c9cac6de5da5b68ebd4d77e2f82ebf
Maksim Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-06 12:22:07 +0100

    made as in tutorial

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
       </div>
-    );
-  }
-}
+    </div>
+  )
+};
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "John",
+      job: "Develoer"
+    },
+    {
+      img: 34,
+      name: "Mike",
+      job: "Develoer"
+    },
+  ];
+
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]} />
+    </section>
+  )
+};
+
+const App = () => (
+  <PersonList />
+);
 
 export default App;
