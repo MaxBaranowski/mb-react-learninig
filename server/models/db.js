6ef2fb358adaa6375fdfb3c89cfb9ddc81a1dc25
Max Baranowski <max.baranowski@mail.ru>
2019-02-11 21:43:25 +0100

    remove server to branch

--- server/models/db.js
+++
@@ -1,7 +0,0 @@
-exports.MongoClient = require('mongodb').MongoClient;
-exports.ObjectId = require('mongodb').ObjectId;
-exports.axios = require('axios');
-
-exports.DB_URI = "mongodb://admin:YaOTg3Z7s8w9vr8YoJTn@ds125945.mlab.com:25945/mb-react-person-list";
-exports.DB_DATABASE_NAME = "mb-react-person-list";
-exports.DB_COLLECTION_NAME = "users-detailed";
