9363383798237f1e5829c66b7000885d33e341ce
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-18 16:20:02 +0100

    gogole api

--- app/src/components/user/detailed/components/Main/Main.js
+++ app/src/components/user/detailed/components/Main/Main.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import styled from "styled-components";
 import Location from "./Location"
+import MapContainer from "./GoogleMap"
 
 export default function Main(props) {
-    const { user } = props;
-    return (
-        <DetailProfileMainWrapper>
-            <h1>USER INFO DETAILED</h1>
-
+  const { user } = props;
+  return (
+    <DetailProfileMainWrapper>
+      <h1>USER INFO DETAILED</h1>
+      {/* 
             <pre>
             {
                 JSON.stringify(user, null, 2)
             }
-            </pre>
-            <Location />
-        </DetailProfileMainWrapper>
-    )
+            </pre> */}
+      <MapContainer />
+      <Location />
+    </DetailProfileMainWrapper>
+  )
 }
 
 const DetailProfileMainWrapper = styled.main`
