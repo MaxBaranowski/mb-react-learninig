77ed189f136a42d14b0f72bc518774375ca42be5
Max Baranowski <max.baranowski@mail.ru>
2019-02-20 23:11:43 +0100

    Updates

---
+++ precache-manifest.add5cbc9f794000f24ad41288b2da812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8b7dfda04e5ad78c82e",
+    "url": "/person_list_project/static/js/runtime~main.c8b7dfda.js"
+  },
+  {
+    "revision": "156f7dd06166d9d11019",
+    "url": "/person_list_project/static/js/main.156f7dd0.chunk.js"
+  },
+  {
+    "revision": "157fb9a2daa8f94a1a8b",
+    "url": "/person_list_project/static/js/2.157fb9a2.chunk.js"
+  },
+  {
+    "revision": "156f7dd06166d9d11019",
+    "url": "/person_list_project/static/css/main.40d16536.chunk.css"
+  },
+  {
+    "revision": "de9d5b5ddfc41b9121e97e48bc0edf2e",
+    "url": "/person_list_project/index.html"
+  }
+];
