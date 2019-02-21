74e7d107a35709a225bed53101005af4f6bc3a71
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-21 12:01:55 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mb-react-learninig/precache-manifest.416092730a7f0aeab1f39bcc2df725bd.js"
+  "/mb-react-learninig/precache-manifest.a7a41166812450fadb8d04e477bd6422.js"
 );
 
 workbox.clientsClaim();
