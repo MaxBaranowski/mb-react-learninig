4fd3530ed92634195dfa6d8f3f3e792f2d2928ea
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-21 13:07:35 +0100

    Updates

--- precache-manifest.a75da79dbe7783913231ff93089b9ad7.js
+++ precache-manifest.a75da79dbe7783913231ff93089b9ad7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6319ccecea99a54a8ba6",
+    "revision": "3962dbd37303cc1a5eb6",
     "url": "/mb-react-learninig/static/css/main.a6487f9c.chunk.css"
   },
   {
-    "revision": "6319ccecea99a54a8ba6",
-    "url": "/mb-react-learninig/static/js/main.6319ccec.chunk.js"
+    "revision": "3962dbd37303cc1a5eb6",
+    "url": "/mb-react-learninig/static/js/main.3962dbd3.chunk.js"
   },
   {
     "revision": "af25190c01023e2c4836",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/mb-react-learninig/static/js/2.1aa1e9bf.chunk.js"
   },
   {
-    "revision": "2346eba1f58b0977138a60dcc1f08542",
+    "revision": "feefdbf0c1f4872353fced983660e7c4",
     "url": "/mb-react-learninig/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mb-react-learninig/precache-manifest.a7a41166812450fadb8d04e477bd6422.js"
+  "/mb-react-learninig/precache-manifest.a75da79dbe7783913231ff93089b9ad7.js"
 );
 
 workbox.clientsClaim();
